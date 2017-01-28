01fabc17c4002a56fa16056ec2bd812cad8c56c7
Shiny-89 <vaati.shinylink@gmail.com>
2017-01-28 17:03:48 +0100

    Adding the store.php file for store management

---
+++ store.php
@@ -0,0 +1,46 @@
+session_start();
+if(isset($_SESSION["id"])){//checking the existence of a session otherwise push back to login page
+	include('includes/head.php');
+?>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="logo" src="images/logo.png" width="136" height="75" style="margin-top: -26px;">
+      </a>
+    </div>
+    <div class="nav navbar-nav navbar-center">
+      <span><?php echo $_SESSION['username']; ?></span>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <!--<li><a href="#">Link</a></li>-->
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Option <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="stock.php">Stock</a></li>
+            <li><a href="#">Finances</a></li>
+            <li><a href="#">Charts</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href=<?php echo "profile.php?id=".$_SESSION['id']." ";?>>Compte</a></li>
+        </ul>
+      </li>
+    </ul>
+
+  </div>
+</nav>
+
+
+
+
+<?php
+	include('includes/foot.php');
+}
+
+else{
+	// redirect user to login page if he is not connected
+	$login_url = "https://moncafe.000webhostapp.com/login.php";
+	header("Location: ".$login_url);
+	die();
+}
+?>
