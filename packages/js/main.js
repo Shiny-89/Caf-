1b3d45ba8b7b3807fdc1936c595827396500259e
Shiny-89 <vaati.shinylink@gmail.com>
2017-01-29 04:53:03 +0100

    adding some style

--- packages/js/main.js
+++ packages/js/main.js
@@ -21,15 +21,15 @@ function adding_window(){
     wind += "<div>";
     wind += "<p> Ajouter un nouveau product</p>";
     wind += "<div class='form-group'><label for='name'>Nom : </label><input type='text' class='form-control' id='name'></div>";
-    wind += "<label>Type</label><div class='radio radio-inline'><label><input type='radio' name='type'>Consomable</label></div>";
+    wind += "<label class='radio control-label'>Type</label><div class='radio radio-inline'><label><input type='radio' name='type'>Consomable</label></div>";
     wind += "<div class='radio radio-inline'><label><input type='radio' name='type'>Gros</label></div>";
     wind += "<label>Ajoutez une image du produit</label><label class='btn btn-default btn-file'>Image <input type='file' name='thumb' hidden></label>";
     wind += "<div class='form-group'><label for='price'>Prix d'achat : </label><input type='text' class='form-control' id='price'></div>";
     wind += "<div class='form-group'><label for='sell'>Prix de vente : </label><input type='text' class='form-control' id='sell'></div>";
     wind += "<div class='form-group'><label for='category'>Category : </label><select id='catefories'><option value='categorie'>Categorie</option><select><input type='text' class='form-control' id='category' placeholder='Ou ajoutez une nouvelle catégorie'></div>";
     wind += "<div class='form-group'><label for='sub-category'>Sous-Category : </label><select id='sub_catefories'><option value='ex-categorie'>Ex-Categorie</option><select><input type='text' class='form-control' id='category' placeholder='Ou ajoutez une nouvelle sous-catégorie'></div>";
-    wind += "<button class='btn btn-default red' type='button' onclick='cancel_add();'>Annuler</button>";
-    wind += "<button class='btn btn-default green' type='button' onclick='add_product();'>Ajouter</button>";
+    wind += "<button class='btn btn-default btn-danger' type='button' onclick='cancel_add();'>Annuler</button>";
+    wind += "<button class='btn btn-default green btn-success' type='button' onclick='add_product();'>Ajouter</button>";
     wind += "</div></div>";
 
     if (!layer){// checking if the layer exist already in the markup
@@ -41,7 +41,7 @@ function adding_window(){
     }
 
       document.getElementsByTagName('body')[0].innerHTML += wind;
-      el('wind').style.display = 'block';
+      el('new_prod').style.display = 'block';
   }
   else {
     el('layer').style.display ='block';
